0b700afd37392b98e98b93f7e24446205aec84eb
Danil <122017946+Liveafun13@users.noreply.github.com>
2024-10-12 16:38:17 +0500

    Update Server.js

--- Server.js
+++ Server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
 
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 8080;
 
 app.use(cors({
     origin: 'https://client-tasks-g8tou6bz1-daniel-popka-potapenkos-projects.vercel.app',  // Замените на точный URL вашего фронтенда
