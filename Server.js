8446e862dca5f4ac005753509ce4944826c07fff
Danil <122017946+Liveafun13@users.noreply.github.com>
2024-11-27 14:21:51 +0500

    Update Server.js

--- Server.js
+++ Server.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
 
-//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 8080;
+//const PORT = process.env.PORT || 8080;
 
 app.use(cors({
     origin: process.env.CLIENT_URL,
